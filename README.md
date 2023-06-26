8c4881fed1cd5df85a7ff001df8f10238381dc0a
Nabil080 <bellilanabil8@gmail.com>
2023-06-26 11:56:41 +0200

    ajout connexion + récupération des prix

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Scrapping
 
 - 'npm install' pour installer les dépendances
+- Renseigner votre email et votre mot de passe de connexion
 - Renseigner l'url de la catégorie à scrapper (pour le site agriestdistribution.fr)
 - Renseigner le nom du fichier à générer
 - Profit ??
--- agriScrap.js
+++ agriScrap.js
@@ -1,28 +1,50 @@
 import puppeteer from "puppeteer";
 import ExcelJS from "exceljs";
 
-if (process.argv.length < 4) {
-  console.log("\x1b[33mVeuillez spécifier les deux paramètres requis :\x1b[0m'")
-  console.log("1.\x1b[33mL'URL\x1b[0m de base.")
-  console.log("2.\x1b[33mLe nom du fichier Excel\x1b[0m dans lequel les données seront sauvegardées.")
-  console.log("Exemple: node agriscrap.js https://agriestdistribution.fr/Bennes-a-grappins--0000792-vente/BENNE-A-GRAPPIN-AP-BG-AVC-DTS-LG-1500-QUICKE-3--0002510.html excel_agriest")
+if (process.argv.length < 6) {
+  console.log("\x1b[33mVeuillez spécifier les quatres paramètres requis :\x1b[0m'")
+  console.log("1.\x1b[33mL'email\x1b[0m de votre compte agriest.")
+  console.log("2.\x1b[33mLe mot de passe\x1b[0m de votre compte.")
+  console.log("3.\x1b[33mL'URL\x1b[0m de base.")
+  console.log("4.\x1b[33mLe nom du fichier Excel\x1b[0m dans lequel les données seront sauvegardées.")
+  console.log("Exemple: node agriscrap.js mon.email@mail.com mdp1234 https://agriestdistribution.fr/Bennes-a-grappins--0000792-vente/BENNE-A-GRAPPIN-AP-BG-AVC-DTS-LG-1500-QUICKE-3--0002510.html excel_agriest")
   process.exit(1);
 }
 
-const baseUrl = process.argv[2];
-const fileName = process.argv[3];
+const loginUrl = 'https://agriestdistribution.fr/user/login'
+const loginEmail = process.argv[2];
+const loginPassword = process.argv[3];
+const baseUrl = process.argv[4];
+const fileName = process.argv[5];
 
 console.log('Bienvenue chez\x1b[33m AgriScrap\x1b[0m !')
 console.log(`\x1b[33mRécupération des données en cours\x1b[0m, l'url de base est : ${baseUrl} .`);
 console.log(`Les données seront sauvegardées dans un fichier\x1b[33m ${fileName}.xlsx\x1b[0m dans le dossier 'results'`);
 
+
+// CONNEXION
+const login = async (page) => {
+  await page.goto(`${loginUrl}`, {
+    waitUntil: "domcontentloaded",
+  });
+  await page.type("#email", loginEmail);
+  await page.type("#password", loginPassword);
+  await page.click(".panel-footer > button");
+
+  await page.waitForNavigation();
+}
+
+
 const getProducts = async () => {
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     defaultViewport: null,
   });
 
   const page = await browser.newPage();
+  
+  // Login before scraping
+  await login(page)
 
   let hasNextPage = true;
   let hasProperties;
@@ -39,8 +61,9 @@ const getProducts = async () => {
       const products = await page.evaluate(() => {
 
           const productTable = document.querySelector(".table-ui-shop-property-table");
+          hasProperties = productTable !== null ? true : false;
 
-          if(productTable !== null){
+          if(hasProperties){
             const productRows = productTable.querySelectorAll("tr")
 
             return Array.from(productRows).map((product) => {
@@ -56,11 +79,13 @@ const getProducts = async () => {
       });
       const productName = await page.evaluate(() => document.querySelector('.ui-shop-nom').innerText)
       const productRef = await page.evaluate(() => document.querySelector('.ui-shop-ref > .ui-shop-ref-value').innerText)
+      const productPrice = await page.evaluate(() => document.querySelector('span.prixtpl > .ht > .value').innerText)
 
-      console.log(productName,productRef)
+      console.log(productPrice)
 
       worksheet.addRow(["Nom",productName])
       worksheet.addRow(["Ref",productRef])
+      worksheet.addRow(["Prix",productPrice])
       products.forEach((product) => {
         worksheet.addRows([product]);
           console.log(product)
